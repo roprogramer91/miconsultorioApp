4d61b8d13def5e1947c63c552df6c3382f3a5d4b
roprogramer91 <roprogramer91@gmail.com>
2026-02-28 20:46:15 -0300

    fix: bind to 0.0.0.0, add cors, accept data loss on push

--- app.js
+++ app.js
@@ -5,14 +5,16 @@ const Router = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middlewares
-app.use(express.json());
+const cors = require('cors');
 
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 // Usamos el router principal
 app.use('/api', Router);
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server corriendo en el puerto: ${PORT}`);
 });
